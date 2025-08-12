7eb810176d80d471482f6090c751d06a29fef422
Aashna1107 <vermaaash07@gmail.com>
2025-08-12 12:14:44 +0530

    Add files via upload

---
+++ A2 ques 3a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int findMissingNumber(int arr[], int n) {
+    int totalSum = n * (n + 1) / 2; 
+    int arraySum = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        arraySum += arr[i];
+    }
+    return totalSum - arraySum; 
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 6}; 
+    int n = 6; 
+    cout << "The missing number is: " << findMissingNumber(arr, n) << endl;
+
+    return 0;
+}
