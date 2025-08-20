94fa3c6a049fa60ca6315c1e9b6033c65ca90612
Aashna1107 <vermaaash07@gmail.com>
2025-08-20 12:17:47 +0530

    Add files via upload

---
+++ A2 extra ques 4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void sort012(int arr[], int n) {
+    int low = 0, mid = 0, high = n - 1;
+
+    while (mid <= high) {
+        switch (arr[mid]) {
+            case 0:
+                swap(arr[low++], arr[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(arr[mid], arr[high--]);
+                break;
+        }
+    }
+}
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements (0s, 1s, 2s only): ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    sort012(arr, n);
+
+    cout << "Sorted array: ";
+    display(arr, n);
+
+    return 0;
+}
+
